20366358ac2db39c2de5b53ad859939b8566dcda
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2021-09-25 11:09:28 -0600

    Agregando los metodos del ciclo de vida de una actividad

--- app/src/main/java/com/example/material_design/MainActivity.java
+++ app/src/main/java/com/example/material_design/MainActivity.java
@@ -33,11 +33,43 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this,"Lista vacia.", Toast.LENGTH_SHORT).show();
             }
         });
+
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        // La actividad está creada.
     }
 
     //Ver datos
     public void mostrarDatos(View view){
         startActivity(new Intent(MainActivity.this, MostrarActivity.class));
 
+    }@Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+        // La actividad está a punto de hacerse visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+        // La actividad se ha vuelto visible (ahora se "reanuda").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+        // Enfocarse en otra actividad  (esta actividad está a punto de ser "detenida").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        // La actividad ya no es visible (ahora está "detenida")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        // La actividad está a punto de ser destruida.
     }
 }
